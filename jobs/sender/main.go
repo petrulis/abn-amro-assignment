e7d27bff82f374c3af6548cb6d2aed4763bdb7c0
Justinas Petrulis <justinas@adapt.dk>
2018-11-05 18:13:05 +0200

    Created queue consumer, fixed sam template

--- jobs/sender/main.go
+++ jobs/sender/main.go
@@ -7,22 +7,61 @@ import (
 	"os"
 	"github.com/aws/aws-sdk-go/aws"
 	"context"
-	"fmt"
 	"github.com/aws/aws-lambda-go/events"
+	"github.com/petrulis/abn-amro-assignment/model"
+	"encoding/json"
+	"github.com/go-errors/errors"
+	"github.com/aws/aws-sdk-go/service/sns"
 )
 
 var ddb *dynamodb.DynamoDB
 var tbl *string
+var snc *sns.SNS
 
 func init() {
 	sess := session.Must(session.NewSession(&aws.Config{Region: aws.String(os.Getenv("REGION"))}))
 	ddb = dynamodb.New(sess)
 	tbl = aws.String(os.Getenv("DDB_TABLE"))
+	snc = sns.New(sess)
+}
+
+func sendSMS(request *model.MessageRequest) error {
+	input := &sns.PublishInput{
+		PhoneNumber: aws.String(request.RecipientIdentifier),
+		Message: aws.String(request.Body),
+	}
+	_, err := snc.Publish(input)
+	return err
+}
+
+func sendEmail(request *model.MessageRequest) error {
+	return nil
+}
+
+func send(request *model.MessageRequest) error {
+	switch request.IdentifierType {
+	case model.IdentifierTypeSMS:
+		return sendSMS(request)
+	case model.IdentifierTypeEmail:
+		return sendEmail(request)
+	default:
+		return errors.New("unsupported message type")
+	}
 }
 
 func Handler(ctx context.Context, event events.SQSEvent) error {
-	fmt.Println(ctx)
-	fmt.Println(event)
+	for _, record := range event.Records {
+		body := record.Body
+		var request model.MessageRequest
+		err := json.Unmarshal([]byte(body), &request)
+		if err != nil {
+			return err
+		}
+		err = send(&request)
+		if err != nil {
+			return err
+		}
+	}
 	return nil
 }
 
